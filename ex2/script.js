9a6e573f957239b538b20de11040e77b79e95fdc
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-04-09 21:45:06 +0100

    add:added ex2

---
+++ ex2/script.js
@@ -0,0 +1,34 @@
+const nomeGuardado = localStorage.getItem("nome");
+
+if (nomeGuardado) {
+  console.log("Nome guardado:", nomeGuardado);
+}
+
+const form = document.getElementById("form-nome");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  const nome = document.getElementById("nome").value;
+
+  localStorage.setItem("nome", nome);
+
+});
+
+
+let theme = localStorage.getItem("theme") 
+let themebutton = document.getElementById("theme-button");
+
+theme ?(themebutton.innerText = theme) : (themebutton.innerText = "light");
+
+themebutton.addEventListener("click", function() {
+  if (theme === "light") {
+    localStorage.setItem("theme", "dark");
+    theme = "dark";
+    themebutton.innerText = "dark";
+  }else{
+    localStorage.setItem("theme", "light");
+    theme = "light";
+    themebutton.innerText = "light";
+  }
+});
