f3a500c833008ed387a78fc2133c9a7c2081a411
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-04-27 19:36:05 +0100

    update:updated

--- docs/script.js
+++ docs/script.js
@@ -119,12 +119,21 @@ function deleteTask(id) {
     saveTask(tasks);
 
     Swal.fire({
-        position: "top-center",
-        icon: "success",
-        title: "Task deleted successfully!",
-        showConfirmButton: false,
-        timer: 1500
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+        });
     });
+
     renderTasks();
 }
 
